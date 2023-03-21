c0d76ab878f83a3a1a77ca44be931baa9ad64373
Vishal <vishal5056315@gmaail.com>
2023-03-21 16:50:40 +0530

    first commmit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Covid from "./components/covid"
+
+const App = () =>{
+  return(
+    <Covid/>
+  )
+}
+export default App
---
+++ src/components/covid.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react'
+
+const Covid= () =>{
+    const getCovidData = async () =>{
+        try{
+            const res = await fetch('https://data.covid19india.org/data.json');
+            const actualData = await  res.json();
+            console.log(actualData)
+        
+        }catch(err){
+            console.log(err);
+        }
+
+    }
+    useEffect(() =>{
+        // getCovidData();
+    }, []);
+    return (
+        <>
+
+        <h1> LIVE</h1>
+        <h2>COVID-19 CORONAVIRUS TRACKER</h2>
+        </>
+    )
+}
+export default Covid
